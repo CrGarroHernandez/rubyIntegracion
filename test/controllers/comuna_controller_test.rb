87f75c69d2a2ab6f413744a815102f464c28d84c
CrGarroHernandez <39251426+CrGarroHernandez@users.noreply.github.com>
2019-04-28 01:04:33 -0400

    Archivos faltantes

---
+++ test/controllers/comuna_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ComunaControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/provincia_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ProvinciaControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
